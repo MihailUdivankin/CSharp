d1e2140f2c8ef5c732d30d8d02d6faa5267bd84b
«Mikhail_Udivankin» <ciklon3@yandex.ru>
2022-04-23 16:43:06 +0400

    Ex003 add random

--- Examples/Ex003/Program.cs
+++ Examples/Ex003/Program.cs
@@ -1,5 +1,5 @@
-﻿int numberA = 3;
+﻿int numberA = new Random().Next(1,10);
 Console.WriteLine(numberA);
-int numberB = 5;
+int numberB = new Random().Next(1,10);
 Console.WriteLine(numberB);
 Console.WriteLine(numberA + numberB);
