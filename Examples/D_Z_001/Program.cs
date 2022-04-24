2d7b6d72b722e2fa20a5bc8089e21d5c1e705d33
«Mikhail_Udivankin» <ciklon3@yandex.ru>
2022-04-24 14:14:29 +0400

    D_Z_001 программа вычисления модуля числа

---
+++ Examples/D_Z_001/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу вычисления модуля числа.
+//2 -> 2  ; -3 -> 3  ; -7 -> 7
+Console.WriteLine("Введите число: ");
+string s_a = Console.ReadLine();
+int a = int.Parse(s_a);
+
+if(a >= 0)
+{
+   Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(a * (-1));
+}
