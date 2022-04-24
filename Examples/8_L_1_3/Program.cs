6fed5ed677d2e2144b0a7de663bd22fc6105cc23
«Mikhail_Udivankin» <ciklon3@yandex.ru>
2022-04-24 12:21:34 +0400

    8_L_1_3

---
+++ Examples/8_L_1_3/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите число дня недели: ");
+string s_a = Console.ReadLine();
+int a = int.Parse(s_a);
+
+if(a == 1)
+{
+   Console.WriteLine("Понедельник"); 
+}
+if(a == 2)
+{
+   Console.WriteLine("Вт"); 
+}
+if(a == 3)
+{
+   Console.WriteLine("Ср"); 
+}
+if(a == 4)
+{
+   Console.WriteLine("Чт"); 
+}
+if(a == 5)
+{
+   Console.WriteLine("Пят"); 
+}if(a == 6)
+{
+   Console.WriteLine("Сб"); 
+}
+if(a == 7)
+{
+   Console.WriteLine("Вс"); 
+}
