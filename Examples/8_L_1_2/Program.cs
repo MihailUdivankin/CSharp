47aa9f71184609ee00688ca083025abf127bd531
«Mikhail_Udivankin» <ciklon3@yandex.ru>
2022-04-24 12:21:12 +0400

    8_L_1_2

---
+++ Examples/8_L_1_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число: ");
+string s_a = Console.ReadLine();
+int a = int.Parse(s_a);
+
+Console.WriteLine("Введите второе число: ");
+string s_b = Console.ReadLine();
+int b = int.Parse(s_b);
+
+if(a*a == b)
+{
+   Console.WriteLine("Второе число является квадратом первого"); 
+}
+else
+{
+    Console.WriteLine("Второе число не является квадратом первого");
+}
