20245991aba55ac6fb231ed22c0e06cc9e8e469c
«Mikhail_Udivankin» <ciklon3@yandex.ru>
2022-04-24 14:46:33 +0400

    D_Z_003 программа меньшее и большее числа

---
+++ Examples/D_Z_003/Program.cs
@@ -0,0 +1,19 @@
+﻿// 1. Напишите программу, которая на вход принимает два числа
+// и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число: ");
+string s_a = Console.ReadLine();
+Console.WriteLine("Введите второе число: ");
+string s_b = Console.ReadLine();
+
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+
+if (a > b)
+{
+    Console.Write("Число " + a + " больше числа " + b);
+}
+else
+{
+    Console.Write("Число " + b + " больше числа " + a);
+}
