670beb47e986656a96bd2481a3b48d5fc1f839c1
«Mikhail_Udivankin» <ciklon3@yandex.ru>
2022-04-24 14:27:54 +0400

    D_Z_002 программа вычисление обратного числа

---
+++ Examples/D_Z_002/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает значение, 
+// а на выходе показывает обратное значение
+// 1 -> 1  ; 2 -> 0.5  ; 0.25 -> 4
+
+Console.WriteLine("Введите число: ");
+string s_a = Console.ReadLine();
+decimal a = int.Parse(s_a);
+
+decimal x = (1 / a);
+Console.WriteLine(x);
