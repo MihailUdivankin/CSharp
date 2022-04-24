75648234d8d27c477a047fed8d4ca397f0f3e816
«Mikhail_Udivankin» <ciklon3@yandex.ru>
2022-04-24 14:01:36 +0400

    8_L_1_5

---
+++ Examples/8_L_1_5/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+string s_a = Console.ReadLine();
+int a = int.Parse(s_a);
+int b = 0-a;
+
+//Console.WriteLine(a);
+//Console.WriteLine(b);
+
+while (b<(a+1))
+{
+    //Console.WriteLine(b);
+    Console.Write($"{b} ");
+    b=b+1;
+}
