3b6960db3929d0fa7a42c96b6338637c9ddf2d2a
«Mikhail_Udivankin» <ciklon3@yandex.ru>
2022-05-15 09:24:39 +0400

    seminar 2 dz 1

---
+++ Examples/S_2_dz_01/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 1: Напишите программу, которая принимает на 
+// вход трёхзначное число и на выходе показывает вторую 
+// цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+int TakeNum(int num)
+{
+    int sec = 0;
+    sec = (num % 100 - num % 10) / 10;
+    return sec;
+}
+Console.WriteLine("Введите трехзначное число");
+string isis=Console.ReadLine();
+int iss = int.Parse(isis);
+Console.WriteLine($"Вторая цифра этого числа {TakeNum(iss)}");
+
