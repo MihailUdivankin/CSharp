2709a7d258754ebd578466bdeb72775b484ed9ea
«Mikhail_Udivankin» <ciklon3@yandex.ru>
2022-05-15 23:42:32 +0400

    seminar 3 dz 1 Проверка на палиндром

---
+++ Examples/S_3_dz_01/Program.cs
@@ -0,0 +1,45 @@
+﻿//  Задача 1: Напишите программу, которая принимает на вход пятизначное число 
+//  и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+bool IsPalindrome(string s)   // программа проверяет является ли число или текст палиндромом
+{
+    for (int i = 0; i < s.Length / 2; ++i)
+    {
+        if (s[i] != s[s.Length - 1 - i]) // !=  это знак неравенства
+        return false;
+    }
+    return true;
+}
+Console.WriteLine("Введите пятизначное число");
+string palin=Console.ReadLine();
+string s = palin;
+
+if((IsPalindrome(s) == true))
+{
+    Console.Write("Введенное число ");
+    Console.ForegroundColor = ConsoleColor.DarkGreen; // устанавливаем цвет
+    Console.Write(s);
+    Console.ResetColor(); // сбрасываем в стандартный
+    Console.Write(" является палиндромом");
+}
+else if((IsPalindrome(s) == false))
+{
+    Console.Write("Введенное число ");
+    Console.ForegroundColor = ConsoleColor.DarkGreen; // устанавливаем цвет
+    Console.Write(s);
+    Console.ForegroundColor = ConsoleColor.Red; // устанавливаем цвет
+    Console.Write(" НЕ является палиндромом");
+    Console.ResetColor(); // сбрасываем в стандартный
+}
+//******
+// {
+//     Console.WriteLine($"ВВеденное число {s} является палиндромом");
+// }
+// else if((IsPalindrome(s) == false))
+// {
+//     Console.WriteLine($"ВВеденное число {s} НЕ является палиндромом");
+// }
+//*****
+//арозаупаланалапуазора     4564567654654
