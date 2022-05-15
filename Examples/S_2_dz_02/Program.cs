85bbdc9fee0a7aa0f2214f374f69decc7cde3724
«Mikhail_Udivankin» <ciklon3@yandex.ru>
2022-05-15 10:07:41 +0400

    seminar 2 dz 2

---
+++ Examples/S_2_dz_02/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 2: Напишите программу, которая выводит 
+// случайное трёхзначное число и удаляет вторую цифру 
+// этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int num1 = 0; // программа выводит сразу пять трехзначных чисел для наглядности
+int num2 = 0;
+void BinarNumber(int num)
+{
+    num1 = num / 100;
+    num2 = num % 10;
+    Console.WriteLine($" ==> {num1}{num2}");
+}
+int index = 0;
+while (index < 5)
+{
+    int number = new Random().Next(100, 1000);
+    Console.Write(number);
+    BinarNumber(number);
+    index++;
+}
+
+// //Второй вариант программы
+// int number = new Random().Next(100, 1000); //простой вариант решения.
+// int num1 = 0;//Выводит одно трехзначное число   
+// int num2 = 0;
+// void BinarNumber(int num)
+// {
+//     num1 = num / 100;
+//     num2 = num % 10;
+//     Console.WriteLine($" ==> {num1}{num2}");
+// }
+// Console.Write(number);
+// BinarNumber(number);
+    
