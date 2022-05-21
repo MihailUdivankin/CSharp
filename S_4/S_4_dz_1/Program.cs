1d6ad7f1f6db831011a6eb3c76b647f2edcd1358
«Mikhail_Udivankin» <ciklon3@yandex.ru>
2022-05-21 19:51:38 +0400

    Seminar 4 dz Exponentiation a and b

---
+++ S_4/S_4_dz_1/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Задача 1: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Exponentiation(int a, int b)  // ++++
+{
+    int all =a;
+    for(int i=1; i<b; i++)
+    {
+        all *= a;
+        //all = all * a;
+    }
+    return all;
+}
+Console.WriteLine(Exponentiation(3,5));
+Console.WriteLine(Exponentiation(2,4));
+Console.WriteLine(Exponentiation(2,5));
