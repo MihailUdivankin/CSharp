09b55d17efaa584538c8d28148db645844a83846
«Mikhail_Udivankin» <ciklon3@yandex.ru>
2022-05-21 20:02:29 +0400

    Seminar 4 dz 2 The sum of the digits of the number

---
+++ S_4/S_4_dz_2/Program.cs
@@ -0,0 +1,25 @@
+﻿//  Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int Sum(int num)    //Программа принимает ввод числа с консоли, преобразует его в строку,
+{                   //вычисляет длину строки (используется в цикле for())
+    int summ=0;
+    string len = num.ToString();
+    for(int j=0;j<len.Length;j++)
+    {
+        int num1 = num % 10;
+        num = num / 10;
+        summ= summ +num1;
+    }
+    return summ;
+}
+Console.WriteLine(Sum(452));
+Console.WriteLine(Sum(82));
+Console.WriteLine(Sum(9012));
+// Далее ввод с консоли
+Console.WriteLine("Введите число");
+string isis=Console.ReadLine();
+int iss = int.Parse(isis);
+Console.WriteLine(Sum(iss));
+
