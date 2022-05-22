8276e7ebb590dbff3c441b4637f55283739c872b
«Mikhail_Udivankin» <ciklon3@yandex.ru>
2022-05-22 18:37:14 +0400

    Seminar 5 dz 1 массив случайных положительных 3-х значных чисел

---
+++ S_5/S_5_dz_1/Program.cs
@@ -0,0 +1,37 @@
+﻿// // Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами.  
+// // Напишите программу, которая покажет количество чётных чисел в массиве. 
+ 
+int [] GetArray(int length, int min, int max) 
+{ 
+    int [] result = new int [length]; 
+    for (int i = 0 ; i< length ; i++) 
+    { 
+        result[i]= new Random().Next(min,max); 
+    } 
+    return result; 
+} 
+ 
+void PrintArray(int[] array) 
+{ 
+    int length = array.Length; 
+    for (int i=0; i<length; i++) 
+    { 
+        Console.Write($"{array[i]}  "); 
+    }      
+    Console.WriteLine(); 
+} 
+ 
+void CountСhet(int[] array) 
+{ 
+int count = 0; 
+for (int i=0; i<array.Length; i++) 
+{ 
+    if (array[i]% 2 == 0)  
+    count ++; 
+} 
+Console.WriteLine(count); 
+} 
+ 
+int [] array = GetArray(6,10,200); 
+PrintArray(array); 
+CountСhet(array);
