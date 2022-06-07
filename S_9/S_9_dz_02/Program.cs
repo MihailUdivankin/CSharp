14c1b961ee10af7c136c7c7be2e3e4ff8a455902
«Mikhail_Udivankin» <ciklon3@yandex.ru>
2022-06-07 22:15:05 +0400

    рекурсия сумма натуральных чисел от m до n

---
+++ S_9/S_9_dz_02/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке 
+// от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+int sum = 0;
+int ValuesSet(int m, int n)
+{
+    if (m == n+1) return sum;
+    sum += m;
+    ValuesSet(m + 1, n); 
+    return sum;
+}
+Console.Write(ValuesSet(1, 15));
